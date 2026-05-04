7498745156e5ec69bc30d86dd88be104d8e951ef
Artem280590 <gonya90.ag@gmail.com>
2026-05-04 19:39:50 +0300

    add fix 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "all.documentreader.filereader.office.vie"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
